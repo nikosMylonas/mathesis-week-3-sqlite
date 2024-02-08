a32dcdd0e4272be5c2c3099339cab6a15b9917bd
nikosMylonas <122935990+nikosMylonas@users.noreply.github.com>
2024-02-08 15:39:10 +0200

    Delete config/dbConfig_Post.mjs

--- config/dbConfig_Post.mjs
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-
-const dbName = process.env.DB_NAME;
-const dbUser = process.env.DB_USERNAME;
-const dbPassword = process.env.DB_PASSWORD;
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-
-export { dbName, dbUser, dbPassword, dbHost, dbPort };
